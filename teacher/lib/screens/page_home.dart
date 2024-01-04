e6bb997406c3871f8034507a70bee0cf8605a9f4
YokotaYusuke <tarut8166@gmail.com>
2024-01-04 14:49:39 +0900

    生徒の一覧表示の編集

--- teacher/lib/screens/page_home.dart
+++ teacher/lib/screens/page_home.dart
@@ -67,77 +67,106 @@ class PageHome extends ConsumerWidget {
       body: Center(
         child: Column(
           children: [
-            TableCalendar(
-              firstDay: DateTime.utc(2022, 4, 1),
-              lastDay: DateTime.utc(2025, 12, 31),
-              eventLoader: getEvent, //追記
-              selectedDayPredicate: (day) {
-                return isSameDay(_selected, day);
-              },
-              onDaySelected: (selected, focused) async {
-                // if (!isSameDay(_selected, selected)) {
-                final f = DateFormat('yyyy-MM-dd');
-                final selectedDate = f.format(selected);
-                debugPrint('selected:$selectedDate');
-                final DayBelongings data =
-                    await getBelongingsApiData(date: selectedDate);
-                debugPrint("data: $data");
-                ref
-                    .read(dateNotifierProvider.notifier)
-                    .updateState("$selected");
-                ref
-                    .read(dayBelongingsNotifierProvider.notifier)
-                    .updateState(data);
-                ref.read(indexNotifierProvider.notifier).updateState(1);
-              },
-              focusedDay: _focused,
+            Container(
+              height: 400,
+              color: Colors.amber,
+              child: TableCalendar(
+                firstDay: DateTime.utc(2022, 4, 1),
+                lastDay: DateTime.utc(2025, 12, 31),
+                eventLoader: getEvent,
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selected, day);
+                },
+                onDaySelected: (selected, focused) async {
+                  final f = DateFormat('yyyy-MM-dd');
+                  final selectedDate = f.format(selected);
+                  debugPrint('selected:$selectedDate');
+                  final DayBelongings data =
+                      await getBelongingsApiData(date: selectedDate);
+                  debugPrint("data: $data");
+                  ref
+                      .read(dateNotifierProvider.notifier)
+                      .updateState("$selected");
+                  ref
+                      .read(dayBelongingsNotifierProvider.notifier)
+                      .updateState(data);
+                  ref.read(indexNotifierProvider.notifier).updateState(1);
+                },
+                focusedDay: _focused,
+              ),
             ),
-            FutureBuilder<List<Map<String, dynamic>>>(
-              future: getStudents(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  // データがまだ取得されていない場合の表示
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  // エラーが発生した場合の表示
-                  return Text('エラー: ${snapshot.error}');
-                } else {
-                  // データを表示
-                  final List<Map<String, dynamic>> data = snapshot.data!;
-                  return Wrap(
-                    children: data
-                            .map((el) => Container(
-                                  margin: const EdgeInsets.all(8.0),
-                                  height: 70,
-                                  width: 100,
+            Container(
+              height: 400,
+              color: Colors.green.shade900,
+              child: Expanded(
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: getStudents(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('エラー: ${snapshot.error}');
+                    } else {
+                      final List<Map<String, dynamic>> data = snapshot.data!;
+                      return GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 10,
+                          crossAxisSpacing: 1.0,
+                          mainAxisSpacing: 1.0,
+                          childAspectRatio: 1.0,
+                        ),
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          final el = data[index];
+                          return Container(
+                            margin: const EdgeInsets.all(1.0),
+                            height: 0,
+                            // width: 2,
+                            decoration: BoxDecoration(
+                              color: el["checkedInventory"]
+                                  ? Colors.green
+                                  : Colors.grey,
+                              // color: Colors.white,
+                              borderRadius: BorderRadius.circular(2),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(Icons.person,
+                                    size: 27, color: Colors.white),
+                                // size: 45,
+                                // color: Colors.white),
+                                const SizedBox(height: 8),
+                                Container(
+                                  width: 60,
+                                  padding: const EdgeInsets.all(2),
                                   decoration: BoxDecoration(
-                                      // color: Colors.grey,
-                                      color: el["checkedInventory"]
-                                          ? Colors.green
-                                          : Colors.grey,
-                                      borderRadius: BorderRadius.circular(10)),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      const Align(
-                                        alignment: Alignment.topCenter,
-                                        child: Icon(Icons.person,
-                                            size: 24, color: Colors.white),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Align(
-                                        alignment: Alignment.bottomCenter,
-                                        child:
-                                            Text(el["student_name"].toString()),
-                                      ),
-                                    ],
+                                      color: Colors.grey.shade700,
+                                      borderRadius: BorderRadius.circular(5.0)),
+                                  child: Text(
+                                    el["student_name"].toString(),
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        fontFamily: "Roboto",
+                                        fontSize: 10.0,
+                                        color: Colors.white),
                                   ),
-                                ))
-                            .toList() ??
-                        [],
-                  );
-                }
-              },
+                                )
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+            Container(
+              height: 60,
+              color: Colors.brown,
             )
           ],
         ),
